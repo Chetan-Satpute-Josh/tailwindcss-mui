880fcfe99833273ab89c31aa9f4fffb8deb43ca2
Chetan Satpute <chetan.satpute@joshsoftware.com>
2024-04-04 12:03:40 +0530

    Tailwind css and MUI setup

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function HomeScreen() {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <h1>Create React App + Tailwindcss + MUI</h1>
+      <Button variant="contained">MUI Button</Button>
+    </div>
+  );
+}
+
+export default HomeScreen;
